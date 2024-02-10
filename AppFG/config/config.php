cfd4d9fc6d11e8ee493ac97376075038b4a930ac
Fuat Görmez <fuatgormez@me.com>
2024-02-10 05:53:18 +0100

    update new

--- AppFG/config/config.php
+++ AppFG/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 date_default_timezone_set('Europe/Berlin');
-$config['base_url'] = 'https://www.onlineiris.com/';
+$config['base_url'] = 'http://onlineiris:8888/';
 
 /*
 |--------------------------------------------------------------------------
--- AppFG/logs/log-2024-02-10.php
+++ AppFG/logs/log-2024-02-10.php
@@ -2,3 +2,10 @@
 
 ERROR - 2024-02-10 01:41:09 --> 404 Page Not Found: Faviconico/index
 ERROR - 2024-02-10 04:01:09 --> 404 Page Not Found: Faviconico/index
+ERROR - 2024-02-10 05:13:49 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'onlineiris'@'localhost' (using password: YES) /Users/fuatgormez/Sites/localhost/onlineiris/SystemFG/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2024-02-10 05:13:49 --> Unable to connect to the database
+ERROR - 2024-02-10 05:13:49 --> 404 Page Not Found: Faviconico/index
+ERROR - 2024-02-10 05:14:30 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'onlineiris'@'localhost' (using password: YES) /Users/fuatgormez/Sites/localhost/onlineiris/SystemFG/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2024-02-10 05:14:30 --> Unable to connect to the database
+ERROR - 2024-02-10 05:17:48 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'onlineiris'@'localhost' (using password: YES) /Users/fuatgormez/Sites/localhost/onlineiris/SystemFG/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2024-02-10 05:17:48 --> Unable to connect to the database
